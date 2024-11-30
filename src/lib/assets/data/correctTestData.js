3d857e711c864aa8f37adf33d0b58b183fd27dc9
CodeWeiName <128158298+CodeWeiName@users.noreply.github.com>
2024-11-30 17:42:17 +0500

    dataTest-correctTranslate

---
+++ src/lib/assets/data/correctTestData.js
@@ -0,0 +1,23 @@
+const translationOfWords = [
+    {id: 1, word: "āпа", correctionTranslate: "люлька"}
+    {id: 2, word: "хāп", correctionTranslate: "лодка"}
+    {id: 3, word: "срысь", correctionTranslate: "море"}
+    {id: 4, word: "тумп", correctionTranslate: "остров"}
+    {id: 5, word: "āны", correctionTranslate: "блюдо"}
+]
+
+const translationOfPhrases = [
+    {id: 1, phrases: "А̄мпын тотвес", correctionTranslate: "Собака унесла"}
+    {id: 2, phrases: "Хот патыс", correctionTranslate: "Ветер стих"}
+    {id: 3, phrases: "Кӯсяррись титнопыл", correctionTranslate: "Бурундук огляделся"}
+    {id: 4, phrases: "Пупакв ла̄ви", correctionTranslate: "Медьведь отвечает"}
+    {id: 5, phrases: "Хо̄талэ ла̄ви", correctionTranslate: "Солнце ответило"}
+]
+
+const translationOfSentences = [
+    {id: 1, sentences: "А̄кврись - а̄кврись, тучаӈын хо̄та?", correctionTranslate: "Бабушка, бабушка, где твой тучан?"}
+    {id: 2, sentences: "Тувыг ēмты мисанув, луванум мгыс пум вруӈкве та вылтахтв", correctionTranslate: "Настанет лето и мы начинаем заготовлять сено для нашего скота"}
+    {id: 3, sentences: "Пупаквēг кӯсярыг о̄лэ̄г.", correctionTranslate: "Живут на свете медведь и бурундук"}
+    {id: 4, sentences: "Хо̄тал сака котарахтас.", correctionTranslate: "Солнце очень сильно грело."}
+    {id: 5, sentences: "Пупакве соваге но̄ӈхаль паттысаге.", correctionTranslate: "Медведь вытаращил глаза, смотрит на поле.}
+]
